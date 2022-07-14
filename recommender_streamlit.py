3b5c95c1ddbdfe90623ccb147c75bc3027f40f66
Dimarfin <d.afanasenkov@gmail.com>
2022-07-14 23:31:25 +0200

    c4

--- recommender_streamlit.py
+++ recommender_streamlit.py
@@ -4,7 +4,7 @@ import sklearn as skl
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
-path = 'ml-latest-small\\'
+path = ''
 
 ratings=pd.read_csv(path+'ratings.csv')
 movies=pd.read_csv(path+'movies.csv')
@@ -21,10 +21,10 @@ def movie_popul_rating(genre, ratings, movies, n=10):
     df1 = df.groupby('movieId').mean()
     df1['count'] = df.groupby('movieId').count()
     
-    scaler = MinMaxScaler()
-    df1_scaled = scaler.fit_transform(df1.to_numpy())
-    df1_scaled = pd.DataFrame(df1_scaled, columns=df1.columns)
-    
+    scaler = MinMaxScaler((0,1))
+    df1_scaled = scaler.fit_transform(df1)
+    df1_scaled = pd.DataFrame(df1_scaled, columns=df1.columns, index=df1.index)
+
     df1['score'] = df1_scaled['rating']*df1_scaled['count']
     
     df2 = df1.sort_values("score", ascending=False).head(n)
@@ -38,7 +38,7 @@ flat_list = [x for xs in all_listed_genres for x in xs]
 genres = sorted(list(set(flat_list)))
 genres = ['all']+genres[1:]+[genres[0]]
  
-st.title("Movie recommender v0.02")
+st.title("Movie recommender v0.04")
  
 st.write("""
 ### Popularity based
