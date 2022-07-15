e1d35c34ea768fd3f3d73fec77b3b93eaefd2af1
Dimarfin <d.afanasenkov@gmail.com>
2022-07-15 11:39:35 +0200

    c5

--- recommender_streamlit.py
+++ recommender_streamlit.py
@@ -32,24 +32,89 @@ def movie_popul_rating(genre, ratings, movies, n=10):
  
     return df3
 
+def find_movie_id(movie_name, df):
+    df = df[df['title'].str.contains(movie_name)]
+    return df['movieId']
+
+def movie_item_coll_filter(movie_id, ratings, movies, n=10):
+    movie_crosstab = pd.pivot_table(data=ratings, 
+                                     values='rating', 
+                                     index='userId', 
+                                     columns='movieId')
+    fillna_val=2.51111
+    movie_crosstab = movie_crosstab.fillna(value=fillna_val)
+    similar_to = movie_crosstab.corrwith(movie_crosstab[movie_id],axis=0)
+    similar_to = pd.DataFrame(similar_to, columns=['PearsonR'])
+    similar_to.sort_values("PearsonR", ascending=False,inplace=True)
+    top_similar = similar_to.head(n+1)
+    top_similar = top_similar[top_similar.index != movie_id]
+    if len(top_similar.iloc[:,0])>n:
+        top_similar = similar_to.head(n)
+        
+    top_similar = top_similar.merge(movies[['movieId','title','genres']], how='left', on='movieId')
+    return top_similar
+
+def movie_user_coll_filter(user_id, ratings, movies, n=10):
+    users_items = pd.pivot_table(data=ratings, 
+                                 values='rating', 
+                                 index='userId', 
+                                 columns='movieId')
+    
+    fillna_val=2.51111
+    users_items = users_items.fillna(value=fillna_val)
+    similarity = pd.DataFrame(cosine_similarity(users_items),
+                           columns=users_items.index, 
+                           index=users_items.index)
+    
+    weights = (similarity.loc[similarity.index!=user_id,user_id]
+               /sum(similarity.loc[similarity.index!=user_id,user_id])
+               )
+    #all users except the one with user_id 
+    #and movies not rated by user_id
+    not_rated = users_items.loc[users_items.index!=user_id, users_items.loc[user_id,:]==fillna_val]
+    weighted_averages = pd.DataFrame(not_rated.T.dot(weights), columns=["predicted_rating"])
+    weighted_averages_top = weighted_averages.sort_values(by='predicted_rating', ascending=False).head(n)
+    weighted_averages_top = weighted_averages_top.merge(movies[['movieId','title','genres']], how='left', on='movieId')
+    return weighted_averages_top
+
 #get genre list
 all_listed_genres = list(movies['genres'].str.split('|'))
 flat_list = [x for xs in all_listed_genres for x in xs]
 genres = sorted(list(set(flat_list)))
 genres = ['all']+genres[1:]+[genres[0]]
  
-st.title("Movie recommender v0.04")
+st.title("Movie recommender v0.06")
  
 st.write("""
 ### Popularity based
 """)
 genre_choise = st.selectbox(
-                      'Which genre would you like?',
+                      'Choose a genre',
                       (genres)
                       )
 popul_df = movie_popul_rating(genre_choise, ratings, movies)
 #st.text(genre_choise)
 st.table(popul_df)
 
+st.write("""
+### Similar to a particular movie
+""")
+movie_name = st.text_input('Movie title or key word', 'Forrest Gump')
+ids = find_movie_id(movie_name, movies)
 
+top_similar = pd.DataFrame()
+for movie_id in ids:
+    top_similar = pd.concat([top_similar, 
+                            movie_item_coll_filter(movie_id,ratings, movies ,n=10)
+                            ], 
+                            axis=0)
+top_similar = top_similar.sort_values('PearsonR') 
+st.table(top_similar.head(10))
 
+st.write("""
+### User based collaborative filtering
+""")
+user_id = st.number_input('Insert a user ID number', min_value=1, max_value=610, value=100, step=1)
+like_user = movie_user_coll_filter(user_id, ratings, movies,n=10)
+ 
+st.table(like_user.head(10))
